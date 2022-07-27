dd54c08c538540424879c75ddfb2feb22f9cac1b
Alecsey Efimov <alicsey2000@gmail.com>
2022-07-27 06:48:25 +0300

    Реализация интерфейса Parcelable классом-хранилищем заметок.

--- app/src/main/java/ru/geekbrains/mynotesapp/model/Note.java
+++ app/src/main/java/ru/geekbrains/mynotesapp/model/Note.java
@@ -1,11 +1,13 @@
 package ru.geekbrains.mynotesapp.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import androidx.annotation.NonNull;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class Note {
+public class Note implements Parcelable {
     private String titleOfNote;
     private String textOfNote;
     private LocalDate createData;
@@ -24,9 +26,30 @@ public class Note {
         this.createData = createData;
     }
 
+    protected Note(Parcel in) {
+        titleOfNote = in.readString();
+        textOfNote = in.readString();
+        createData = (LocalDate) in.readSerializable();
+    }
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+
     @NonNull
     @Override
     public String toString() {
+        if (titleOfNote.equals("")) {
+            return "note " + (notes.indexOf(this) + 1);
+        }
         return getTitleOfNote();
     }
 
@@ -56,5 +79,17 @@ public class Note {
 
     public void setCreateData(LocalDate createData) {
         this.createData = createData;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        parcel.writeString(titleOfNote);
+        parcel.writeString(textOfNote);
+        parcel.writeSerializable(createData);
     }
 }
