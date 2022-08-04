0b9f3bb4e80264d1848e3477396cae8408616fa6
Alecsey Efimov <alicsey2000@gmail.com>
2022-08-04 11:10:32 +0300

    Создание контекстного меню на фрагменте со списком заметок

--- app/src/main/java/ru/geekbrains/mynotesapp/ui/NotesFragment.java
+++ app/src/main/java/ru/geekbrains/mynotesapp/ui/NotesFragment.java
@@ -1,12 +1,13 @@
 package ru.geekbrains.mynotesapp.ui;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import ru.geekbrains.mynotesapp.Presenter;
@@ -65,8 +66,27 @@ public class NotesFragment extends Fragment {
             }
         });
 
+        registerForContextMenu(listView);
+
         return rootView;
     }
 
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
 
+        requireActivity().getMenuInflater().inflate(R.menu.notes_popup_menu, menu);
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        if (item.getItemId() == R.id.context_menu_delete) {
+            presenter.onClickDeleteNote(info.position);
+            return true;
+        }
+        return false;
+    }
 }
